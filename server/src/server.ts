fdd72fa9e7be2c4b0f9c74824c2d4450b49f3394
EuHiroshi <hiroshivz001@gmail.com>
2023-05-19 01:13:55 -0300

    dia 2 rocketseat parte 2

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,21 +1,16 @@
 import fastify from 'fastify'
-import { PrismaClient } from '@prisma/client'
+import cors from '@fastify/cors'
+import { memoriesRoutes } from './routes/memories'
 
 const app = fastify()
-const prisma = new PrismaClient()
 
 // HTTP Method: GET, POST, PUT, PUT, DELETE
 
-app.get('/users', async () => {
-  const users = await prisma.user.findMany()
-
-  return users
+app.register(cors, {
+  origin: true,
 })
+app.register(memoriesRoutes)
 
-app
-  .listen({
-    port: 3333,
-  })
-  .then(() => {
-    console.log('🚀 HTTP server running on http://localhost:3333')
-  })
+app.listen({ port: 3333 }).then(() => {
+  console.log('🚀 HTTP server running on http://localhost:3333')
+})
